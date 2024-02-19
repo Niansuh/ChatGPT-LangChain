dab8ed754acf10203d83b14ac3a43321ddce5e79
Husnain <139365289+Niansuh@users.noreply.github.com>
2024-02-19 06:06:54 -0500

    Change Sidebar Name

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,7 +174,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT | Niansuh
+          ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
           AI assistant.
